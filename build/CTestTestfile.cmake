5fecbe54c5978e6b249f27344a3f499ee087d9cc
cadanny <carlos.flores@ucb.edu.bo>
2023-11-26 14:18:43 -0400

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubunturos/catkin_ws/src
+# Build directory: /home/ubunturos/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("learning_tf2-master")
+subdirs("test")
+subdirs("beginner_tutorials")
+subdirs("urdf_examples")
+subdirs("mastering_ros_robot_description_pkg-master")
---
+++ build/urdf_examples/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubunturos/catkin_ws/src/urdf_examples
+# Build directory: /home/ubunturos/catkin_ws/build/urdf_examples
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
